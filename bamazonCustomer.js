27766c77e00cbc094cd3ef543ede85cfed2b71b2
brandonconover90@gmail.com <brandonconover90@gmail.com>
2019-04-23 23:58:28 -0600

    first commit pseudocode finished

---
+++ bamazonCustomer.js
@@ -0,0 +1,15 @@
+// immediately display all items for sale(the table, basically)
+
+// prompt user: 
+
+// Ask about ID of desired product
+
+// Ask how many units
+
+// Customer selects
+
+// if selection greater than/equal to amount in inventory: 
+//      fulfill order: UPDATE sql database (subtract req amnt from current amnt)
+//                     show customer total cost of purchase
+
+// else "Insufficient Quantity" and stop order
