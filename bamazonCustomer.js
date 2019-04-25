d30e85fb993cba25fcdfae48cf9a14eab936d1c0
brandonconover90@gmail.com <brandonconover90@gmail.com>
2019-04-24 20:58:29 -0600

    skeleton functions copypasted from activities and my own started

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,15 +1,59 @@
+// require mysql and inquirer
+var mysql = require("mysql");
+var inquirer = require("inquirer");
+// create connection for SQL server 
+// USE GITIGNORE/.ENV?? TO KEEP MY PASSWORD SECRET
+// var connection = mysql.createConnection({
+//     host: "localhost",
+
+//     // Your port;
+//     port: 3375,
+
+//     // Your username
+//     user: "root",
+
+//     // Your password **FIX!!!!!!**
+//     password: "",
+//     database: "top_songsDB"
+// });
+// ------------------------------------------
 // immediately display all items for sale(the table, basically)
 
-// prompt user: 
+// ------------------------------------------
+function userPurchase() {
+    // prompt user: (INQUIRER)
+    inquirer
+        .prompt({
+            // Ask about ID of desired product
+            message: "What is the ID of the Item you wish to purchase?"
+        })
+        // Ask how many units
 
-// Ask about ID of desired product
+        .then(function (answer) {
+                // how to grab itemID? 
+                var query = "SELECT item_ID, product_name, department_name, price, stock_quantity FROM products WHERE ?";
+                connection.query(query, {
+                    artist: answer.artist
+                }, function (err, res) {
+                    // from top5000 activity
+                    // for (var i = 0; i < res.length; i++) {
+                    //     console.log("Position: " + res[i].position + " || Song: " + res[i].song + " || Year: " + res[i].year);
+                    // }
+                    // runSearch();
+                });
+            }
+            // ------------------------------------------
+            // Customer selects
 
-// Ask how many units
+            // if selection greater than/equal to amount in inventory: 
+            //      fulfill order: UPDATE sql database (subtract req amnt from current amnt)
+            //                     show customer total cost of purchase
 
-// Customer selects
+            // else "Insufficient Quantity" and stop order
 
-// if selection greater than/equal to amount in inventory: 
-//      fulfill order: UPDATE sql database (subtract req amnt from current amnt)
-//                     show customer total cost of purchase
-
-// else "Insufficient Quantity" and stop order
+            // ------------------------------------------
+            // exit
+            // **********************Review/Extras********************************
+            // NOTE: Call functions!!!
+            // OPTIONAL: prompt for another purchase or to exit 
+            //              HOW: update original "on-load" and run that function after purchase.
